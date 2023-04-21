2c33fa51cc3fde6b4ba9ac094c3d579b4fc672dc
priyansh <priyanshg124@gmail.com>
2023-04-21 19:25:47 +0530

    Creating Project
    Creating and sending files to repository

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+// // const userName = document.querySelector("#email");
+// // const password = document.querySelector("#password");
+// const loginBtn = document.querySelector(".loginBtn");
+// const lForm = document.querySelector(".loginForm");
+
+// loginBtn.addEventListener("click", () => {
+
+// });
---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+const server = express();
+const PORT = 4444;
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.join(__dirname, "public")));
+
+// server.get("/signup", )
+server.post("/login", (req, res) => {});
+
+server.listen(PORT, () => {
+    console.log("http://localhost:" + PORT);
+});
